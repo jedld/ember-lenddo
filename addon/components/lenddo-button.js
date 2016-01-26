3e0eeb8def333e003f8d4d9de932d11015240f61
Joseph Dayo <joseph.dayo@gmail.com>
2016-01-27 01:35:47 +0800

    Moved lenddo js component for better Ember integration

--- addon/components/lenddo-button.js
+++ addon/components/lenddo-button.js
@@ -21,4 +21,40 @@ export default Ember.Component.extend({
   },
   mode: 'popup',
   layout: layout,
+
+  didInsertElement() {
+    let _this = this;
+    window.LenddoVerifyConfig = {
+      action: `${this.get('mode')}`,
+      scriptId: `${this.get('partnerScriptId')}`,
+      clientId: `${this.get('clientId')}`,
+      name: `${this.get('buttonType')}`,
+      fb_redirect: this.get('fbRedirect'),
+      verificationFields: {
+        /* sample verification field values */
+        firstname:  `${this.get('verificationFields.firstName')}`,
+        middlename: `${this.get('verificationFields.middleName')}`,
+        lastname: `${this.get('verificationFields.lastName')}`,
+        birthdate: `${this.get('verificationFields.lastName')}`,
+        email: `${this.get('verificationFields.email')}`,
+        employer: `${this.get('verificationFields.employer')}`,
+        mobilephone: `${this.get('verificationFields.mobileNumber')}`,
+        university: `${this.get('verificationFields.university')}`
+      },
+      onSubmit: function(cb) {
+        _this.sendAction('onClick', this);
+        var errors = false;
+        if (errors === false) {
+          cb();
+        }
+      },
+    };
+
+    let la = document.createElement('script');
+    la.type = 'text/javascript';
+    la.async = true;
+    la.src = 'https://authorize.partner-service.link/verify.js?v=' + Date.now();
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(la, s);
+  },
 });
